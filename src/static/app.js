85335be7a879df01ba63e4356b690b2891bf8a53
Salvatore <79525903+salvatorebonanno93@users.noreply.github.com>
2026-06-11 13:07:22 +0000

    chore: trigger step 3

--- src/static/app.js
+++ src/static/app.js
@@ -128,9 +128,10 @@ document.addEventListener("DOMContentLoaded", () => {
       }, 5000);
     } catch (error) {
       messageDiv.textContent = "Failed to sign up. Please try again.";
-      messageDiv.className = "error";
+      messageDiv.className = "Error";
       messageDiv.classList.remove("hidden");
-      console.error("Error signing up:", error);
+      console.error("Error signing up:", Error);
+
     }
   });
 
